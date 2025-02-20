f607be436d27aede44ce221276092043ee2b4430
cha-law <char_law@outlook.com>
2025-02-20 09:14:37 +0000

    Added grid structure to webpage and extra javascript functions

--- script.js
+++ script.js
@@ -13,7 +13,15 @@ function Board() {
     // Function to retrieve the entire board.
     const getBoard = () => board; 
 
-    return {getBoard};
+    const outputBoard = () => {
+        board.forEach((row) => {
+            row.forEach((column) => {
+                console.log(column.getMarkValue())
+            })
+        });
+    }
+
+    return {getBoard, outputBoard};
 }
 
 
@@ -21,7 +29,8 @@ function Cell() {
     let value = "";
 
     const addMark = (player) => {
-        value = player;
+        if (value != "") return;
+        value = player.mark;
     }
 
     const getMarkValue = () => value;
@@ -29,17 +38,17 @@ function Cell() {
 }
 
 
-function GameController() {
+function GameController(player1Name = "Player 1", player2Name = "Player Two") {
     const board = Board();
 
     // Players
     const players = [
         {
-            name: "Player1",
+            name: player1Name,
             mark: "X"
         },
         {
-            name: "Player2",
+            name: player2Name,
             mark: "O"
         },
     ]
@@ -52,5 +61,30 @@ function GameController() {
     }
 
     const getCurrentPlayer = () => currentPlayer;
+
+    const newRound = () => {
+        board.outputBoard();
+        console.log(`${getCurrentPlayer().name}'s turn!`);
+
+        currentColumn = prompt("Enter column (0-2): ");
+        currentRow = prompt("Enter row (0-2): ");
+
+        playRound(currentRow, currentColumn);
+    }
+
+    const playRound = (row, column) => {
+        const board = Board().getBoard();
+        board[column][row].addMark(getCurrentPlayer().mark);
+        // Win check needed
+        changePlayerTurn();
+        newRound();
+    }
+
+    // Start the game.
+    //newRound();
+
+    return {changePlayerTurn, getCurrentPlayer, newRound, playRound};
 }
+
+const game = GameController();
 
